a8bbc3f164c9e5a0d05fc10ad9d66eb7657dce42
Franco Coronel <55667889+Francoz0ma@users.noreply.github.com>
2020-10-29 23:25:16 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//una variable llamada primera con un valor numérico = 40
+//una variable llamada segunda con un valor numérico = 16
+//una variable llamada sumatotal con un valor numérico de la suma de las dos variables anteriores.
+
+let primera = 40;
+let segunda = 16;
+
+let sumatotal;
+
+sumatotal= primera+segunda;
+
+document.getElementById("suma").innerHTML = sumatotal;
